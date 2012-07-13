27e917f7cf818c6c9a07b86d5ea1d0cfd2a3a490
Jasl <jasl9187@hotmail.com>
2012-07-13 23:22:35 +0800

    fix a rule

--- validator_core.js
+++ validator_core.js
@@ -123,7 +123,7 @@ Validator.add_rule('exclusion', {
 Validator.add_rule('email', {
   shoulda : function(value, pattern) {
     if (value === "") {
-
+      return true;
     }
     var regex = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
     return regex.test(value);
